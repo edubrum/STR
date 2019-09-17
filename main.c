5c043735b7663344f784fb65b02eaa0ba461e562
edubrum <edulbrum@gmail.com>
2019-09-17 10:48:27 -0300

    attemping

--- main.c
+++ main.c
@@ -8,11 +8,42 @@ typedef struct {
 	int p;
 	int d;
 	int c_now;
-	int lack;
+	int slack;
 	int priority;
 } Task;
 
-int is_iddle(struct Task task[],int n_tasks){
+struct Task scheduler;
+struct Task task[MAX_N];
+
+void lst_sched(Task *task, int i,int t){
+	int d = find_act_deadline(task,i,t);
+	task.[i].slack = d - task[i].c_now - t;
+	if((task[i].slack < scheduler.slack)||((task[i].slack == scheduler.slack)&&( task[i].priority < scheduler.priority))){
+		scheduler.slack = task[i].slack;
+		scheduler.id = task[i].id;
+		scheduler.priority = task[i].priority;
+		task[i].c_now = task[i].c_now -1;
+	}
+}
+
+int find_act_deadline(Task *task,int i,int t){
+	int n =0;
+	while((task[i].d + task[i].p*n)<= t){
+		n++;
+	}
+	return task[i].d + task[i].p*n;
+}
+
+int find _act_period(){
+	int n =0;
+	while((task[i].p*n)<= t){
+		n++;
+	}
+	return task[i].p*n;
+}
+
+
+int is_iddle( Task *task,int n_tasks){
 	int i, iddle = 0;
 	for(i =0; i< n_tasks;i++)
 		if(task[i].c_now == 0)
@@ -24,17 +55,8 @@ int is_iddle(struct Task task[],int n_tasks){
 	return iddle;
 }
 
-int find_act_period(){
-
-}
-
-int find _act_deadline(){
-
-}
-
 int main(void){
 	int n_tasks = 0, time =0, i,n_preemp = 0,n_ctx_stc = 0,t;
-	Task task[MAX_N];
 	char tsk_brd[MAX_T + 1];
 	printf("\n############################\n");
 	printf("# Welcome to LST Simulator #\n");
@@ -52,6 +74,7 @@ int main(void){
 			task[i].c_now = task[i].c;//c_now changes throught time increment 
 			task[i].priority = i+1;// priority is set by process arrival
 		}
+		scheduler.
 		printf("\nTasks added: ");
 		for(i = 0; i < n_tasks; ++i){
 			printf("T(%c) = [(C:%d);(P:%d);(D:%d)] ",task[i].id,task[i].c,task[i].p,task[i].d);
@@ -65,7 +88,7 @@ int main(void){
 				tsk_brd[t] = '.';
 			else{
 				for(i = 0; i < n_tasks; ++i) {
-							
+					lst_sched(task,i,t);
 				}
 			}
 		}
